5f60da3ed7eedb5c42d6b7cbd54c85f0f1459a8b
Eminks <1908412+eminks@users.noreply.github.com>
2021-10-30 02:00:51 +0000

    Upload files

---
+++ src/Controller/MailController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\MailRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Carbon\Carbon;
+
+class MailController extends AbstractController
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @Route("/mail", name="mail")
+     */
+    public function index(MailRepository $mailList): Response
+    {
+
+        $list = $mailList->findAll();
+        
+        foreach ($list as $key) {
+            if (Carbon::now()->greaterThanOrEqualTo($key->getDate())) {
+                $entityManager = $this->getDoctrine()->getManager();
+
+                $this->SendMail($key->getUserID());
+
+                $entityManager->remove($key);
+                $entityManager->flush();
+        
+            }
+        }
+
+        return $this->json([
+            'status' => "ok"
+        ]);
+    }
+
+
+    private function SendMail($id)
+    {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+
+        $transport = Transport::fromDsn('smtp://@smtp.mailtrap.io:2525?encryption=tls&auth_mode=login');
+        $mailer = new Mailer($transport);
+
+        $email = (new Email())
+        ->from('hello@example.com')
+        ->to($user->getEmail())
+        ->subject('Welcome!')
+        ->text('Welcom to example.com!');
+
+        $mailer->send($email);
+        
+        return;
+    }
+
+    /**
+     * @Route("/maillist", name="mail_list")
+     */
+    public function maillist(MailRepository $mailList): Response
+    {        
+        return $this->json([
+            'users' => $mailList->findAll()
+        ]);
+    }
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Username;
+
+    /**
+     * @ORM\Column(type="string", length=11)
+     */
+    private $Tc;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Address;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $PhoneNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->Username;
+    }
+
+    public function setUsername(string $Username): self
+    {
+        $this->Username = $Username;
+
+        return $this;
+    }
+
+    public function getTc(): ?string
+    {
+        return $this->Tc;
+    }
+
+    public function setTc(string $Tc): self
+    {
+        $this->Tc = $Tc;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->Address;
+    }
+
+    public function setAddress(string $Address): self
+    {
+        $this->Address = $Address;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->PhoneNumber;
+    }
+
+    public function setPhoneNumber(string $PhoneNumber): self
+    {
+        $this->PhoneNumber = $PhoneNumber;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+}
